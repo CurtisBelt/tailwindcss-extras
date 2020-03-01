db2a1544de19a2b51e677a36e3cdcbb58ff57017
Curtis Belt <curtis@curtisbelt.dev>
2020-02-29 21:29:16 -0600

    feat: add aspect-ratio utilities

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const plugin = require('tailwindcss/plugin')
 
-module.exports = plugin(function({ addUtilities }) {})
+module.exports = plugin(function({ addUtilities }) {
+  const newUtilities = {
+    '.aspect-ratio-square': {
+      paddingTop: '100%',
+    },
+    '.aspect-ratio-16x9': {
+      paddingTop: '56.2%',
+    },
+    '.aspect-ratio-21x9': {
+      paddingTop: '42.8%',
+    },
+    '.aspect-ratio-4x3': {
+      paddingTop: '75%',
+    },
+    '.aspect-ratio-2x3': {
+      paddingTop: '150%',
+    },
+  }
+
+  addUtilities(newUtilities, {
+    variants: ['responsive', 'hover'],
+  })
+})
--- tests/generated-utilities.spec.js
+++ tests/generated-utilities.spec.js
@@ -31,3 +31,91 @@ const generatePluginCss = (config) => {
 expect.extend({
   toMatchCss: cssMatcher,
 })
+
+test('utilities are generated', () => {
+  return generatePluginCss().then((css) => {
+    expect(css).toMatchCss(`
+      .aspect-ratio-square {
+        padding-top: 100%
+      }
+
+      .aspect-ratio-16x9 {
+        padding-top: 56.2%
+      }
+
+      .aspect-ratio-21x9 {
+        padding-top: 42.8%
+      }
+
+      .aspect-ratio-4x3 {
+        padding-top: 75%
+      }
+
+      .aspect-ratio-2x3 {
+        padding-top: 150%
+      }
+
+      .hover\\:aspect-ratio-square:hover {
+        padding-top: 100%
+      }
+
+      .hover\\:aspect-ratio-16x9:hover {
+        padding-top: 56.2%
+      }
+
+      .hover\\:aspect-ratio-21x9:hover {
+        padding-top: 42.8%
+      }
+
+      .hover\\:aspect-ratio-4x3:hover {
+        padding-top: 75%
+      }
+
+      .hover\\:aspect-ratio-2x3:hover {
+        padding-top: 150%
+      }
+
+      @media (min-width: 640px) {
+        .sm\\:aspect-ratio-square {
+          padding-top: 100%
+        }
+
+        .sm\\:aspect-ratio-16x9 {
+          padding-top: 56.2%
+        }
+
+        .sm\\:aspect-ratio-21x9 {
+          padding-top: 42.8%
+        }
+
+        .sm\\:aspect-ratio-4x3 {
+          padding-top: 75%
+        }
+
+        .sm\\:aspect-ratio-2x3 {
+          padding-top: 150%
+        }
+
+        .sm\\:hover\\:aspect-ratio-square:hover {
+          padding-top: 100%
+        }
+
+        .sm\\:hover\\:aspect-ratio-16x9:hover {
+          padding-top: 56.2%
+        }
+
+        .sm\\:hover\\:aspect-ratio-21x9:hover {
+          padding-top: 42.8%
+        }
+
+        .sm\\:hover\\:aspect-ratio-4x3:hover {
+          padding-top: 75%
+        }
+
+        .sm\\:hover\\:aspect-ratio-2x3:hover {
+          padding-top: 150%
+        }
+      }
+    `)
+  })
+})
